0519272cc29e34d09ecc41bf60c7f27f80084f4e
Abigail Dillehay <ard99@yahoo.com>
2023-03-04 22:19:44 -0500

    Creating the placeholder Home Page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import IUPUILogo from './images/IUPUILogo.jpg';
+import jaguar from './images/stuffed-jaguar.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className = "upper-header">
+        <img className="IU-logo" src={IUPUILogo} alt="IUPUI Logo"/>
+        <div className = "nav-wrapper">
+          <a href="#">News</a>
+          <a href="#">Events</a>
+          <a href="#">About & Contact</a>
+          {/* fake search button here */}
+        </div>
+      </div>
+      <header className="student-affairs-header">Division of <b>Student Affairs</b></header>
+      <nav>
+        <a href="#">Get Involved</a>
+        <a href="#">Campus Center</a>
+        <a href="#">Health & Wellness</a>
+        <a href="#">Housing</a>
+        <a href="#">Advocacy & Resources</a>
+        <a href="#">Student Conduct</a>
+      </nav>
+      <img className = "jaguar-image" src={jaguar} alt="Stuffed Jaguar"/>
     </div>
   );
 }
