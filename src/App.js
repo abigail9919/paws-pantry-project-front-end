120a06c3b902ff2256f49a21850746f13d07cdb4
Abigail Dillehay <ard99@yahoo.com>
2023-03-06 11:41:26 -0500

    Putting content into placeholder main page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import IUPUILogo from './images/IUPUILogo.jpg';
 import jaguar from './images/stuffed-jaguar.jpg';
+import search from './images/search.png';
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
           <a href="#">News</a>
           <a href="#">Events</a>
           <a href="#">About & Contact</a>
+          <a href="#"><img src={search}/></a>
           {/* fake search button here */}
         </div>
       </div>
@@ -24,8 +26,50 @@ function App() {
         <a href="#">Student Conduct</a>
       </nav>
       <img className = "jaguar-image" src={jaguar} alt="Stuffed Jaguar"/>
+
+      <article>
+        <div className="word-content">
+          <div className = "breadcrumbs">
+            <a href="#">Home</a> / 
+            <a href="#">Advocacy & Resources</a> /
+            <a href="#">Food and Clothing Services</a>
+            <br />
+            <div className="paws-title">PAWS' PANTRY</div>
+          </div>
+
+          <h1>Welcome to the Pantry</h1>
+          <h3>How to shop for free food and personal hygiene products</h3>
+
+          <h4>Here’s what you need to know to make your visit to the pantry a breeze:</h4>
+          <ul>
+            <li>Clients experiencing food insecurity can physically shop in the pantry, as you would in a store, for your own items. There is a check-in and check-out process, and a limit system is in place to allow for everyone to get what they need.</li>
+            <li>Appointments are required to be made 24 hours in advance. Each appointment will allow 15 minutes for shopping.</li>
+            <li><i>Every effort will be made to provide appointments for those with food insecurity. If you are unable to make an appointment, please email jagsfood@iu.edu and a one-time emergency pre-packaged food bag will be provided.</i></li>
+            <li>Every client, even if it is not your first time visiting the pantry, will need to fill out an intake form for the 2022-23 school year.</li>
+          </ul>
+        </div>
+
+        <div className="location-and-hours">
+          <h2>Location and Hours</h2>
+          <h3>Paws’ Pantry – Campus Center 130</h3>
+
+          Fall 2023 Hours <br /><br />
+
+          All appointments must be made online 24 hours in advance. <br /><br />
+
+          Tuesdays (students only)<br />
+          10 a.m.–4 p.m. <br /><br />
+
+          Wednesdays (students only)<br />
+          11 a.m.–5 p.m.<br /><br />
+
+          Thursdays (students, faculty, and staff)<br />
+          12noon–6 p.m.
+        </div>
+      </article>
+
+
     </div>
   );
 }
-
 export default App;
