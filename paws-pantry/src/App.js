ba5c4a340ddd97a02d3ee97e22bb55e1b94be028
Joseu-19 <95296240+Joseu-19@users.noreply.github.com>
2023-04-04 17:47:33 -0400

    space

--- paws-pantry/src/App.js
+++ paws-pantry/src/App.js
@@ -8,7 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> 
+        
         <a
           className="App-link"
           href="https://reactjs.org"
