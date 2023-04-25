39377c21f506f4cfb7d5f23cfc1266140502bd69
Tessica Mac <jessicamac48@yahoo.com>
2023-04-25 00:12:43 -0400

    FINALLY

--- src/pages/Forms/Forms.js
+++ src/pages/Forms/Forms.js
@@ -28,67 +28,53 @@ function Forms() {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-
-        setClientID(event.target.elements.clientID.value);
-        setFirstName(event.target.elements.firstName.value);
-        setLastName(event.target.elements.lastName.value);
-        setEmail(event.target.elements.email.value);
-        setStreetAddress(event.target.elements.street_address.value);
-        setAddressLine2(event.target.elements.Address_line_2.value);
-        setCity(event.target.elements.city.value);
-        setEmployment(employment);
-        setStanding(selectedOption);
-        setZipCode(event.target.elements.zip_code.value);
-        setState(event.target.elements.state.value);
-        setCounty(event.target.elements.county.value);
-        setNumAdults(event.target.elements.numAdults.value);
-        setNumChildren05(event.target.elements.numChildren05.value);
-        setNumChildren618(event.target.elements.numChildren618.value);
-
-        if (!selectedOption) {
-            alert('Please select one option.');
-        } 
-        else if (!employment){
-            alert('Please select an employment status.');
-        }
-        else if (!firstName){
-            setFirstNameError(true);
-            return;
-        }
-        else {
-            setFirstNameError(false);
-            axios.post("http://localhost:8000/api/v1/clients/newclients", {
-                clientID: clientID,
-                firstName: firstName,
-                lastName: lastName,
-                email: email,
-                standing: standing,
-                street_address: street_address,
-                Address_line_2: Address_line_2,
-                city: city,
-                zip_code: zip_code,
-                state: state,
-                county: county,
-                numAdults: numAdults,
-                numChildren05: numChildren05,
-                numChildren618: numChildren618,
-                employment: employment
-            
-                })
-                .then((response) => {
-                alert('New Client added successfully!');
-                console.log(response);
-                window.location.reload();
-                })
-                .catch((error) => {
-                alert("There was an issues creating new client");
-                console.log(error.response.data.message);
-                setErrorMessage(error.response.data.message);
-            });
-        }
-
+        const formData = new FormData(event.target);
+      
+        // extract form values from formData object
+        const clientID = formData.get('clientID');
+        const firstName = formData.get('firstName');
+        const lastName = formData.get('lastName');
+        const email = formData.get('email');
+        const street_address = formData.get('street_address');
+        const Address_line_2 = formData.get('Address_line_2');
+        const city = formData.get('city');
+        const employment = formData.get('employment');
+        const standing = formData.get('standing');
+        const zip_code = formData.get('zip_code');
+        const state = formData.get('state');
+        const county = formData.get('county');
+        const numAdults = formData.get('numAdults');
+        const numChildren05 = formData.get('numChildren05');
+        const numChildren618 = formData.get('numChildren618');
+      
+        axios.post("http://localhost:8000/api/v1/clients/newclients", {
+          clientID: clientID,
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+          standing: standing,
+          street_address: street_address,
+          Address_line_2: Address_line_2,
+          city: city,
+          zip_code: zip_code,
+          state: state,
+          county: county,
+          numAdults: numAdults,
+          numChildren05: numChildren05,
+          numChildren618: numChildren618,
+          employment: employment
+        })
+        .then((response) => {
+          alert('New Client added successfully!');
+          console.log(response);
+          window.location.reload();
+        })
+        .catch((error) => {
+          alert("There was an issues creating new client");
+          console.log(error.response.data.message);
+          setErrorMessage(error.response.data.message);
+        });
     };
-
 
     return (
         <div className='forms-home'>
@@ -133,7 +119,7 @@ function Forms() {
                         onChange={() => setSelectedOption('faculty')}
                     />
                     <label htmlFor="faculty">Faculty</label><br /><br />
-                    <label for="faculty">Faculty</label><br /><br />
+                    <label htmlFor="faculty">Faculty</label><br /><br />
 
 
                     {/* NAME */}
@@ -143,67 +129,66 @@ function Forms() {
                         id="first-name"
                         name="firstName"
                         placeholder="First"
-                        size="5"
                         required
-                        minlength="1"
-                        maxlength="60"
+                        minLength="1"
+                        maxLength="60"
                         onChange={(e) => {
                             setFirstName(e.target.value);
                             setFirstNameError(false);
                         }}
                     />                    
                     {firstNameError && <p className="error">Please enter a valid first name</p>}
-                    <input type="text" id="last-name" name="lastName" placeholder="Last" size="10" required minlength="1" maxlength="60"></input>
+                    <input type="text" id="last-name" name="lastName" placeholder="Last" required minLength="1" maxLength="60"></input>
                     <br /><br />
 
                     {/* UNIVERSITY ID NUMBER */}
                     <label><b>University ID Number *</b></label><br />
-                    <input type="text" class="long" id="uni-id" name="clientID" required minlength="1" maxlength="60"></input>
+                    <input type="text" className="long" id="uni-id" name="clientID" required minLength="1" maxLength="60"></input>
                     <br /> <br />
 
                     {/* IUPUI EMAIL */}
                     <label><b>IUPUI Email Address *</b></label><br />
-                    <input type="text" class="long" id="email" name="email" required minlength="1" maxlength="60"></input>
+                    <input type="text" className="long" id="email" name="email" required minLength="1" maxLength="60"></input>
                     <br /><br />
 
                     {/* ADDRESS */}
                     <label><b>Address *</b></label><br />
-                    <label class="little-label">Street Address</label><br />
-                    <input type="text" class="long" id="street-address" name="street_address" required minlength="1" maxlength="60"></input>
+                    <label className="little-label">Street Address</label><br />
+                    <input type="text" className="long" id="street-address" name="street_address" required minLength="1" maxLength="200"></input>
                     <br />
                     
-                    <label class="little-label">Address line 2</label><br />
-                    <input type="text" class="long" id="street-address-2" name="Address_line_2" minlength="1" maxlength="60"></input>
+                    <label className="little-label">Address line 2</label><br />
+                    <input type="text" className="long" id="street-address-2" name="Address_line_2" minLength="1" maxLength="200"></input>
                     <br />
 
-                    <label class="little-label">City</label>
-                    <label class="little-label label-positioning">State/Province/Region</label>
+                    <label className="little-label">City</label>
+                    <label className="little-label label-positioning">State/Province/Region</label>
                     <br />
-                    <input type="text" class="medium" id="city" name="city" required minlength="1" maxlength="60"></input>
-                    <input type="text" class="medium" id="state" name="state" required minlength="1" maxlength="60"></input>
+                    <input type="text" className="medium" id="city" name="city" required minLength="1" maxLength="200"></input>
+                    <input type="text" className="medium" id="state" name="state" required minLength="1" maxLength="200"></input>
                     <br />
 
-                    <label class="little-label">Postal/Zip Code</label>
-                    <label class="little-label different-label-positioning">Country</label>
+                    <label className="little-label">Postal/Zip Code</label>
+                    <label className="little-label different-label-positioning">Country</label>
                     <br />
-                    <input type="text" class="medium" id="postal" name="zip_code" required minlength="1" maxlength="60"></input>
-                    <input type="text" class="medium" id="country" name="county" required minlength="1" maxlength="60"></input>
+                    <input type="text" className="medium" id="postal" name="zip_code" required minLength="1" maxLength="60"></input>
+                    <input type="text" className="medium" id="country" name="county" required minLength="1" maxLength="200"></input>
                     <br />
 
                     <h1>How many people are in your household?</h1>
                     <label><b>Number of Adults *</b></label><br />
-                    <input type="text" class="long" id="num-adults" name="numAdults" required minlength="1" maxlength="60"></input>
+                    <input type="text" className="long" id="num-adults" name="numAdults" required minLength="1" maxLength="60"></input>
                     <br /><br />
 
                     <label><b>Number of Children, 0-5 years old *</b></label><br />
-                    <input type="text" class="long" id="num-children" name="numChildren05" required minlength="1" maxlength="60"></input>
+                    <input type="text" className="long" id="num-children" name="numChildren05" required minLength="1" maxLength="60"></input>
                     <br /><br />
 
                     <label><b>Number of Children, 6-18 years old *</b></label><br />
-                    <input type="text" class="long" id="num-older-children" name="numChildren618" required minlength="1" maxlength="60"></input>
+                    <input type="text" className="long" id="num-older-children" name="numChildren618" required minLength="1" maxLength="60"></input>
                     <br /><br />
 
-                    <div class="dots"></div>
+                    <div className="dots"></div>
 
                     <label><b>What is your employment status? *</b></label><br />
                     <input type="radio" id="full-time" name="employment" value="Full-time" checked={employment === 'Full-time'} onChange={() => setEmployment('Full-time')} />
@@ -216,13 +201,13 @@ function Forms() {
                     <label htmlFor="disabled">Disabled</label><br />
                     <br /><br />
 
-                    <div class="dots"></div>
+                    <div className="dots"></div>
 {/* 
                     <label><b>Current source of income/resources: click all that apply. This information will not affect your eligibility to use the pantry. *</b></label>
                     <input type="checkbox" id="income-source" name="income-source"></input> */}
 
                     <button type="submit">Save</button>
-                    {errorMessage && <p className="error">{errorMessage}</p>}
+                    {errorMessage && <p classNameName="error">{errorMessage}</p>}
                 </form>
             </article>
         </div>
