a402d844b60b22fb65170f2bb3b6b2a75441908c
Tessica Mac <jessicamac48@yahoo.com>
2023-04-25 11:41:59 -0400

    Appointment page

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -6,8 +6,33 @@ import LocationHours from '../components/LocationHours/LocationHours';
 import Socials from '../components/Socials/Socials';
 import Footer from '../components/Footer/Footer';
 import Button from '../components/Button/Button';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function Search() {
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+      
+        // extract form values from formData object
+        const clientID = formData.get('clientID');
+        const firstName = formData.get('firstName');
+      
+        axios.get( `http://localhost:8000/api/v1/clients/${clientID}/${firstName}`, {
+
+        })
+        .then((response) => {
+          console.log(response.data[0]);
+        })
+        .catch((error) => {
+          alert("There was an issues getting client data");
+          console.log(error.response.data.message);
+          setErrorMessage(error.response.data.message);
+        });
+    };
+
     return (
         <div className = "search-page-wrapper">
             {<SearchBar />}
@@ -22,16 +47,23 @@ function Search() {
                     <br />
                 </div>
 
-                <h1>Appointment Search</h1>
+                <h1 className='searchTitle'>Appointment Search</h1>
+                <h2 className='searchText'>Have You Visited the Pantry Already? Find Your Information Here!</h2>
 
-                <form id="student-search" action="#" method="get">
+                <form id="student-search" onSubmit={handleSubmit}>
                     <label>IU ID Number: </label>
-                    <input type="text" id="student-id" name="ClientID" size="20" required minLength="10" placeholder='200*******' aria-label='Enter IU ID Number'></input><br/><br/>
-                <br></br>
+                    <br></br>
+                    <input type="text" id="student-id" name="clientID" size="20" required minLength="10" placeholder='200*******' aria-label='Enter IU ID Number'></input><br/><br/>
+                    <br></br>
                     <label>First Name: </label>
-                    <input type="text" id="studentName" name="studentName" size="20" required minLength="10" placeholder='First Name' aria-label='Enter Name'></input><br/><br/>
-                    {<Button title="Search" location="/Forms"/>}
-                    {<Button title="Search (Upcoming)" location="/UpcomingAppointment"/>}
+                    <br></br>
+                    <input type="text" id="studentName" name="firstName" size="20" required minLength="1" placeholder='First Name' aria-label='Enter Name'></input><br/><br/>
+                    {/* {<Button title="Search" type='submit' location="/UpcomingAppointment"/>} */}
+                    <button type="submit">Search</button>
+                    <br></br>
+                    <br></br>
+                    <h2 className='searchText'>If You're Visiting the Pantry for the First Time, Please Start a New Application Here.</h2>
+                    {<Button title="Start New Application" location="/Forms"/>}
                 </form>
             </article>
             {<Socials />}
--- src/pages/UpcomingAppointment/UpcomingAppointment.js
+++ src/pages/UpcomingAppointment/UpcomingAppointment.js
@@ -6,8 +6,26 @@ import LocationHours from '../components/LocationHours/LocationHours';
 import Socials from '../components/Socials/Socials';
 import Footer from '../components/Footer/Footer';
 import Button from '../components/Button/Button';
+import '../UpcomingAppointment/Accordion.css'; 
+import React, { useState, useEffect } from "react";
+
+
 
 function UpcomingAppointment() {
+    const [isOpen, setIsOpen] = useState(false);
+    const [isOpen2, setIsOpen2] = useState(false);
+    const [isOpen3, setIsOpen3] = useState(false);
+
+    const toggleAccordion = () => {
+        setIsOpen(!isOpen);
+    };
+    const toggleAccordion2 = () => {
+        setIsOpen2(!isOpen2);
+    };
+    const toggleAccordion3 = () => {
+        setIsOpen3(!isOpen3);
+    };
+    
     return (
         <div >
             {<SearchBar />}
@@ -15,19 +33,51 @@ function UpcomingAppointment() {
             <article>
             {<AsideNav />}
             {<LocationHours />}
-                <div className = "breadcrumbs">
+                {/* <div className = "breadcrumbs">
                     <a href="#">Home</a> / 
                     <a href="#">Advocacy & Resources</a> /
                     <a href="#">Upcoming Appointment</a>
                     <br />
+                </div> */}
+
+                <div className='apptContent'>
+                    <h1>Upcoming Appointment</h1>
+
+                    <h1>Create New Appointment</h1>
+                    <div className="accordion-container">
+                        <div className="accordion-header" onClick={toggleAccordion}>
+                            <h3 className="accordion-title">View Appointments for Tuesday</h3>
+                            <span className="accordion-icon">{isOpen ? '-' : '+'}</span>
+                        </div>
+                        {isOpen && (
+                            <div className="accordion-content">
+                            <p className="accordion-text">content</p>
+                            </div>
+                        )}
+                    </div>
+                    <div className="accordion-container">
+                        <div className="accordion-header" onClick={toggleAccordion2}>
+                            <h3 className="accordion-title">View Appointments for Wednesday</h3>
+                            <span className="accordion-icon">{isOpen2 ? '-' : '+'}</span>
+                        </div>
+                        {isOpen2 && (
+                            <div className="accordion-content">
+                            <p className="accordion-text">content</p>
+                            </div>
+                        )}
+                    </div>
+                    <div className="accordion-container">
+                        <div className="accordion-header" onClick={toggleAccordion3}>
+                            <h3 className="accordion-title">View Appointments for Thursday</h3>
+                            <span className="accordion-icon">{isOpen3 ? '-' : '+'}</span>
+                        </div>
+                        {isOpen3 && (
+                            <div className="accordion-content">
+                            <p className="accordion-text">content</p>
+                            </div>
+                        )}
+                    </div>
                 </div>
-
-                <h1>Upcoming Appointment</h1>
-
-                <form id="student-search" action="#" method="get">
-                    {<Button title="Edit Appointment" location="/Forms"/>}
-                    
-                </form>
             </article>
             {<Socials />}
             {<Footer />}
